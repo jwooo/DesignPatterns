f9ba41395a46e161520caa6a38888752cbddd18e
jwooo <parkjwoo0515@gmail.com>
2023-12-12 23:22:08 +0900

    feat: SaveTextFileOperation 클래스 추가

---
+++ src/behavior/command/command/SaveTextFileOperation.java
@@ -0,0 +1,16 @@
+package behavior.command.command;
+
+import behavior.command.receiver.TextFile;
+
+public class SaveTextFileOperation implements TextFileOperation {
+    private TextFile textFile;
+
+    public SaveTextFileOperation(TextFile textFile) {
+        this.textFile = textFile;
+    }
+
+    @Override
+    public String execute() {
+        return textFile.save();
+    }
+}
