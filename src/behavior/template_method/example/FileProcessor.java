19cddf71ff7b03e143890c9ea18ec2ddc38dfc38
jwooo <parkjwoo0515@gmail.com>
2023-12-19 15:23:04 +0900

    feat: template_method 예제 코드 추가

---
+++ src/behavior/template_method/example/FileProcessor.java
@@ -0,0 +1,31 @@
+package behavior.template_method.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public abstract class FileProcessor {
+    private String path;
+
+    public FileProcessor(String path) {
+        this.path = path;
+    }
+
+    public final int process() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            int result = getResult();
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+                result = calculate(result, Integer.parseInt(line));
+            }
+
+            return result;
+        } catch (IOException e) {
+            throw new IllegalArgumentException(path + "에 해당하는 파일이 없습니다.", e);
+        }
+    }
+
+    protected abstract int calculate(int result, int number);
+    protected abstract int getResult();
+}
